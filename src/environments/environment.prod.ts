7825534b9e6f2261c5c3076eb52c768947b0f60b
Joan Sebastian Saavedra Perafan <joan.saavedra@correounivalle.edu.co>
2024-09-25 00:07:13 -0500

    hola

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  baseURL:'http://localhost:3000/api',
-  baseURLIMG:'http://localhost:3000/data/uploads',
-  SOCKET_ENDPOINT: 'http://localhost:3000'
-};
